36ae88f6aa0bec92c365bf06049c37c2c1ddcd07
Webakov <webakov@yandex.ru>
2025-12-09 16:12:40 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,25 +1,35 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+  : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      // основной origin (localhost / прод)
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
 
         return {
-          hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
+          hostname: url.hostname,
+          port: url.port || undefined,
         }
       }),
+      // твой дев-сервер по IP
+      {
+        protocol: 'http',
+        hostname: '10.0.85.2',
+        port: '3000',
+        // можно без pathname, но так ещё точнее:
+        // pathname: '/api/media/**',
+      },
     ],
   },
+
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
@@ -29,6 +39,7 @@ const nextConfig = {
 
     return webpackConfig
   },
+
   reactStrictMode: true,
   redirects,
 }
